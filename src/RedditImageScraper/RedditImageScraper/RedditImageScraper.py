8170e7eaa81d28618f5cacaeda1cf53a7f58a1c4
James Dorrington <jdorrington@hotmail.com>
2020-08-30 10:39:06 +0100

    Working

--- src/RedditImageScraper/RedditImageScraper/RedditImageScraper.py
+++ src/RedditImageScraper/RedditImageScraper/RedditImageScraper.py
@@ -1,26 +1,29 @@
+import praw # https://praw.readthedocs.io/en/latest/
 import argparse
-
-#https://www.reddit.com/r/redditdev/comments/ah4itf/python_webscraping_images_from_reddit/
-#https://praw.readthedocs.io/en/latest/
+from os import sys, path
+from urllib.parse import urlparse
+import requests
+import configparser
 
 def main():
     parser = argparse.ArgumentParser(description="Download images from reddit")
-    parser.add_argument("sub_reddits",
+    parser.add_argument("-r",
                         metavar="sub_reddits",
                         type=str,
                         nargs="+",
+                        required="true",
                         help="list of subreddits")
-    parser.add_argument('--total_subreddit',
+    parser.add_argument('-st',
                         metavar="total_subreddit",
                         default=100,
                         type=int,
                         help="total images per subreddit to download")
-    parser.add_argument('--total',
+    parser.add_argument('-t',
                         metavar="total",
                         type=int,
                         default=1000,
                         help="total images overall to download")
-    parser.add_argument('--folder',
+    parser.add_argument('-f',
                         metavar="folder",
                         type=str,
                         default="download",
@@ -28,12 +31,30 @@ def main():
 
     args = parser.parse_args()
     
-    print(args.sub_reddits)
-    print(args.total_subreddit)
-    print(args.total)
-    print(args.folder)
-    print("foo")
+    print("sub_reddits = ", args.r)
+    print("total_subreddit = ", args.st)
+    print("total = ", args.t)
+    print("folder = ", args.f)
 
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    reddit = praw.Reddit(client_id=config['REDDIT']['client_id'],         # From https://www.reddit.com/prefs/apps/
+                         client_secret=config['REDDIT']['client_secret'], # From https://www.reddit.com/prefs/apps
+                         user_agent="testscript by /u/jddddddddddd")
+
+    #for submission in reddit.subreddit("dogpictures").hot(limit=None):
+    for submission in reddit.subreddit("dogpictures").hot(limit=10):
+        if(submission.url.endswith(("jpg", "jpeg", "png"))):
+            a = urlparse(submission.url)
+            b = path.basename(a.path)
+            r = requests.get(submission.url, allow_redirects=True)
+            open(b, 'wb').write(r.content)
+            print(b)
+            
+    
+    print("Done!")
 ###############################################
 # Startup
 ###############################################
